f6a8bbb0e02fbed99f40cceb402d2aad046373a6
cc0407 <cc0407@live.ca>
2021-05-16 12:53:18 -0400

    created get location test

--- src/views/LandingPage/index.tsx
+++ src/views/LandingPage/index.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
 
 export const Landing = () => {
+    const [coords, setCoords] = useState([null,null]);
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            console.log("Latitude is :", position.coords.latitude);
+            console.log("Longitude is :", position.coords.longitude);
+        });
+    });
     return (
         <div className="text-6xl font-mono">Landing</div>
     )
