e278c75fc1f7fd9829a199b3329564bc68dc7218
cc0407 <cc0407@live.ca>
2021-05-24 11:19:57 -0400

    created different views
    added 5day view, with centered boxes in a row.

--- src/views/LandingPage/index.tsx
+++ src/views/LandingPage/index.tsx
@@ -10,14 +10,37 @@ export const Landing = () => {
         });
     });
     return (
+        fiveDay()
+    )
+}
+
+const overview = () => {
+    return (
         <div className="screen">
-            <div className="Row">
-                <div className="Col items-stretch">
-                    <RoundRect width={2} height={3}/>
-                    <RoundRect width={2} height={3}/>
+                <div className="Row">
+                    <div className="Col items-stretch">
+                        <RoundRect width={2} height={3}/>
+                        <RoundRect width={2} height={3}/>
+                    </div>
+                    <RoundRect width={'auto'} height={'auto'}/>
                 </div>
-                <RoundRect width={'auto'} height={'auto'}/>
-            </div>
+        </div>
+    )
+}
+
+const fiveDay = () => {
+    return (
+        <div className="screen items-center justify-center">
+                <div className="w-full pb-8 Row items-center justify-center">
+                    <div>This is the five day forecast.</div>
+                </div>
+                <div className="Row">
+                    <RoundRect width={1.5} height={1.5}/>
+                    <RoundRect width={1.5} height={1.5}/>
+                    <RoundRect width={1.5} height={1.5}/>
+                    <RoundRect width={1.5} height={1.5}/>
+                    <RoundRect width={1.5} height={1.5}/>
+                </div>
         </div>
     )
 }
